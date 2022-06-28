1f03f4a8624d07bc6fe28d18f6af088b863eb681
haroldboi <preston.harry2003@gmail.com>
2022-06-28 14:56:20 +0100

    commit

--- public/main.js
+++ public/main.js
@@ -5,6 +5,16 @@ btn.addEventListener('click', () => {
   window.alert('Hi!')
 })
 
+const switcher = document.getElementById("switch");
+switcher.addEventListener('click', () => {
+  let newImage =document.getElementById("imageNew").value;
+  if (newImage !== ""){
+    document.getElementById("image").src = newImage;
+  } else {
+    document.getElementById("imagePhrase").innerHTML = "Enter a valid URL to change the image";
+    }
+})
+
 const enter = document.getElementById('enter');
 enter.addEventListener('click', () => {
   var userName = document.getElementById("name").value;
