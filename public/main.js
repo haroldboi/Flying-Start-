b61b0c19a2504ba37666bc86cc7179a9aa9cdfc2
haroldboi <preston.harry2003@gmail.com>
2022-06-28 14:56:05 +0100

    commit

--- public/main.js
+++ public/main.js
@@ -1,5 +1,13 @@
+console.log("Hello World")
 const btn = document.getElementById('hello-btn')
 
 btn.addEventListener('click', () => {
-  window.alert('Hello world!')
+  window.alert('Hi!')
+})
+
+const enter = document.getElementById('enter');
+enter.addEventListener('click', () => {
+  var userName = document.getElementById("name").value;
+  var nameT = `Hello ${userName}!`
+document.getElementById("namePop").innerHTML = nameT;
 })
